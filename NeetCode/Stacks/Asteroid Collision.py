e7de40852d70444fdfe2e8725ecd496232650b71
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-09-16 22:21:28 -0300

    Stacks - Asteroid Collision

---
+++ NeetCode/Stacks/Asteroid Collision.py
@@ -0,0 +1,18 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        
+        for ast in asteroids:
+            while stack and ast < 0 and stack[-1] > 0:
+                if abs(ast) > stack[-1]:
+                    stack.pop()
+                elif abs(ast) == stack[-1]:
+                    stack.pop()
+                    break
+                else:
+                    break
+            else:       
+                stack.append(ast)    
+            
+                    
+        return stack
