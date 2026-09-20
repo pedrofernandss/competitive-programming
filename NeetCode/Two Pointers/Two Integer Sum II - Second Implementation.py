58c5148c3320de95f615f773520a822aa546bc53
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-09-20 17:18:23 -0300

    Two Integer Sum II - Second Implementation

---
+++ NeetCode/Two Pointers/Two Integer Sum II - Second Implementation.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left_pointer = 0
+        right_pointer = len(numbers)-1
+
+        while left_pointer < right_pointer:
+            soma = numbers[left_pointer] + numbers[right_pointer]
+            
+            if soma > target:
+                right_pointer -= 1
+            elif soma < target:
+                left_pointer += 1
+            else:
+                return [left_pointer+1, right_pointer+1]
